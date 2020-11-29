ddbf0aff1503d359bfd2672e08446d642bb2bf0e
Suraj Thapa <stsurajst@gmail.com>
2020-11-30 00:01:54 +0545

    front end camera complete

--- App.js
+++ App.js
@@ -1,11 +1,182 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+  Image,
+} from 'react-native';
+import { Camera } from 'expo-camera';
+
+const uploadEndPoint =
+  'https://kxugngk07d.execute-api.ap-southeast-2.amazonaws.com/dev/image-upload';
 
 export default function App() {
+  const [startCamera, setStartCamera] = React.useState(false);
+  const [previewVisible, setPreviewVisible] = React.useState(false);
+  const [capturedImage, setCapturedImage] = React.useState(null);
+  const [cameraType, setCameraType] = React.useState(
+    Camera.Constants.Type.back
+  );
+
+  const cameraRef = React.useRef(null);
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    console.log(status);
+    if (status === 'granted') {
+      setStartCamera(true);
+    } else {
+      Alert.alert('Access denied');
+    }
+  };
+  const __takePicture = async () => {
+    let photo;
+    if (cameraRef) {
+      try {
+        photo = await cameraRef.current.takePictureAsync();
+      } catch (error) {
+        console.log(error);
+      }
+
+      console.log(photo);
+      setPreviewVisible(true);
+      //setStartCamera(false)
+      setCapturedImage(photo);
+    }
+  };
+  const __savePhoto = () => {
+    console.log('save photo');
+    // let formData = new FormData();
+    // const { height, uri, width } = capturedImage;
+    // formData.append('photo', {
+    //   uri,
+    //   name: `photo.jpg`,
+    //   type: `image/jpg`,
+    //   height,
+    //   width,
+    // });
+    // let options = {
+    //   method: 'POST',
+    //   body: formData,
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // };
+    // try {
+    //   return fetch(uploadEndPoint, options);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
+  const __retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+    __startCamera();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {startCamera ? (
+        <View
+          style={{
+            flex: 1,
+            width: '100%',
+          }}
+        >
+          {previewVisible && capturedImage ? (
+            <CameraPreview
+              photo={capturedImage}
+              savePhoto={__savePhoto}
+              retakePicture={__retakePicture}
+            />
+          ) : (
+            <Camera
+              // type={cameraType}
+              // flashMode={flashMode}
+              style={{ flex: 1 }}
+              ref={cameraRef}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  width: '100%',
+                  backgroundColor: 'transparent',
+                  flexDirection: 'row',
+                }}
+              >
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    flexDirection: 'row',
+                    flex: 1,
+                    width: '100%',
+                    padding: 20,
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <View
+                    style={{
+                      alignSelf: 'center',
+                      flex: 1,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={__takePicture}
+                      style={{
+                        width: 70,
+                        height: 70,
+                        bottom: 0,
+                        borderRadius: 50,
+                        backgroundColor: '#fff',
+                      }}
+                    />
+                  </View>
+                </View>
+              </View>
+            </Camera>
+          )}
+        </View>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={__startCamera}
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40,
+            }}
+          >
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +190,79 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const CameraPreview = ({ photo, retakePicture, savePhoto }) => {
+  console.log('previewData>>', photo);
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <ImageBackground
+        source={{ uri: photo && photo.uri }}
+        style={{
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            padding: 15,
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <TouchableOpacity
+              onPress={retakePicture}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4,
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20,
+                }}
+              >
+                Re-take
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={savePhoto}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4,
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20,
+                }}
+              >
+                save photo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
